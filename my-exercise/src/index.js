ac02cd284659baa6a604aa049ead8b77bfdcc93c
selamalex <121111132+selamalex@users.noreply.github.com>
2023-09-04 03:58:39 +0300

    few changes

--- my-exercise/src/index.js
+++ my-exercise/src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+
+import IncDec from './IncreaseDecrease';
 import Count from './MyCounter';
-// import reportWebVitals from './reportWebVitals';
+import EffectTitle from './UseEffectForTitle';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Count/>
+    <IncDec/>
+    <EffectTitle/>
+
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+
